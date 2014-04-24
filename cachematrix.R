77358e0e2fc14a864c0441f2f77b474c48fe65e2
Josh Gough <jsgough@gmail.com>
2014-04-24 18:49:51 -0400

    Added documentation

--- cachematrix.R
+++ cachematrix.R
@@ -1,8 +1,11 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These functions help you calculate, cache, and quickly look up the inverse of matrix. If you
+## need to calculate the inverse of a large matrix multiple times, using these functions can help
+## you reduce the time required.
+##
+## 
+# x <- makeCacheMatrix(matrix(c(1,3,2,4), nrow=2, ncol=2))
 
-## Write a short comment describing this function
-
+## Returns an object that wraps a numeric matrix and can store a cached inverse of that matrix.
 makeCacheMatrix <- function(x = matrix()) {
         i <- NULL
         set <- function(y) {
@@ -20,8 +23,8 @@ makeCacheMatrix <- function(x = matrix()) {
         )
 }
 
-## Write a short comment describing this function
-
+## Given a CacheMatrix x, cacheSolve will attempt to retrieve the pre-cached inverse of x's wrapped matrix. 
+## If no cached inverse is found, it calculates the inverse, stores it in x, and returns the inverse matrix.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
         i <- x$getInverse()
@@ -29,10 +32,9 @@ cacheSolve <- function(x, ...) {
                 message("getting cached inverse")
                 return(i)
         }
-        data <- x$get()
-        i <- solve(data, ...)
+        wrappedMatrix <- x$get()
+        i <- solve(wrappedMatrix, ...)
         x$setInverse(i)
         i
 }
 
-# x <- makeCacheMatrix(matrix(c(1,3,2,4), nrow=2, ncol=2))
